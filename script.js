b10c8a53617acac27acbc964fc6c4e2937d7f9bc
Jasnoor Kaur <jasnoorkaurjasi@gmail.com>
2026-01-31 11:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const typed = document.getElementById("typed");
 
 // Step 1: Fake error → doubt
 setTimeout(() => {
-  systemLine.textContent = "But somehow, it didn’t fail.";
+  ssystemLine.textContent = "Unexpected persistence detected.";
 }, 2200);
 
 // Step 2: Reveal scroll + content
